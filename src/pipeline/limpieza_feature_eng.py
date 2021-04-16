2479f7b663c671f1a967469a71c7b7e321d56972
Mario Heredia <mhnk77@gmail.com>
2021-04-16 15:49:38 -0500

    agrega funciones de limpieza de datos

--- src/pipeline/limpieza_feature_eng.py
+++ src/pipeline/limpieza_feature_eng.py
@@ -1,4 +1,5 @@
 import pickle
+#import pandas as pd #parche
 
 from src.utils.general import get_pickle_from_s3_to_pandas, get_file_path_
 
@@ -9,28 +10,117 @@ class DataCleaner:
 
     def __init__(self, historic=False, query_date=None):
         self.df = get_pickle_from_s3_to_pandas(historic, query_date)
+        #parche
+        #self.df = pd.DataFrame(pickle.load(open('temp/consecutive-inspections-2021-03-15.pkl', 'rb')))
+        #end parche
         self.historic = historic
         self.query_date = query_date
         self.prefix = 'clean'
+        
+##Agregar nuevo codigo
+        
+    def _subset_cols(self):
+        self.df = self.df[['inspection_id', 'facility_type', 'risk', 'zip', 'inspection_date','inspection_type', 'results', 'violations', 'latitude', 'longitude']]
+
+    def _fill_nas(self):
+        self.df['zip']=self.df['zip'].fillna(0)
+        self.df[['facility_type', 'risk', 'inspection_type','violations']]=self.df[['facility_type', 'risk', 'inspection_type','violations']].fillna('na')
 
     def _change_data_types(self):
-        self.df['License #'] = self.df['License #'].fillna(0)
-        self.df['Zip'] = self.df['Zip'].fillna(0)
-        self.df = self.df.astype({"License #": 'int', "Zip": 'int'})
-        self.df = self.df.astype({"License #": 'str', "Zip": 'str'})
+        self.df = self.df.astype({"zip": 'int'})
+        self.df = self.df.astype({"zip": 'str'})
+     
+    def _clean_results(self):
+        self.df['results'].mask(self.df['results'] != 'Pass', other = 'Not Pass', inplace=True)
 
-    def _standarize_column_names(self, excluded_punctuation=".,-*¿?¡!#"):
-        self.df.columns = self.df.columns.str.lower().str.replace(" ", "_")
+    #def _standarize_column_names(self, excluded_punctuation=".,-*¿?¡!#"):
+        #self.df.columns = self.df.columns.str.lower().str.replace(" ", "_")
+        #for ch in excluded_punctuation:
+        #    self.df.columns = self.df.columns.str.replace(ch, "")
+
+    def _standarize_column_strings(self, columns, excluded_punctuation=".,-*'¿?¡!()", gap_punct="\/"):
+        for col in columns:
+            self.df[col] = self.df[col].apply(lambda x: x.lower().replace(" ", "_"))
+            self.df[col] = self.df[col].apply(lambda x: x.lower().replace("á", "a"))
+            self.df[col] = self.df[col].apply(lambda x: x.lower().replace("é", "e"))
+            self.df[col] = self.df[col].apply(lambda x: x.lower().replace("í", "i"))
+            self.df[col] = self.df[col].apply(lambda x: x.lower().replace("ó", "o"))
+            self.df[col] = self.df[col].apply(lambda x: x.lower().replace("ú", "u"))
+            self.df[col] = self.df[col].apply(lambda x: x.lower().replace("ü", "u"))
+        
         for ch in excluded_punctuation:
-            self.df.columns = self.df.columns.str.replace(ch, "")
+            self.df[col] = self.df[col].apply(lambda x: x.replace(ch, ""))
+            
+        for ch in gap_punct:
+            self.df[col] = self.df[col].apply(lambda x: x.replace(ch,"_"))
+            
 
-        self.df = self.df.rename(columns={'license_':'license'})
+    def _clean_facility_type(self):
+        
+        self.df['facility_type'] = self.df['facility_type'].apply(lambda x: x.replace('restuarant', 'restaurant'))
+        self.df['facility_type'] = self.df['facility_type'].apply(lambda x: x.replace('theatre', 'theater'))
+        self.df['facility_type'] = self.df['facility_type'].apply(lambda x: x.replace('herabal', 'herbal'))
+        self.df['facility_type'] = self.df['facility_type'].apply(lambda x: x.replace('day_care', 'daycare'))
+        self.df['facility_type'] = self.df['facility_type'].apply(lambda x: x.replace('long_term', 'longterm'))
+        
+        self.df.loc[self.df['facility_type'].str.contains('childern|children|1023|5_years_old', case=False, na=None), 'facility_type'] = 'childrens_service_facility'
+        self.df.loc[self.df['facility_type'].str.contains('conv|mart|gas_station_store', case=False, na=None), 'facility_type'] = 'convenience_store'
+        self.df.loc[self.df['facility_type'].str.contains('assis|longterm|nursing|supportive', case=False, na=None), 'facility_type'] = 'assisted_living'
+        self.df.loc[self.df['facility_type'].str.contains('herbal_life|herbalife|herbalcal', case=False, na=None), 'facility_type'] = 'herbalife'
+        self.df.loc[self.df['facility_type'].str.contains('after_school', case=False, na=None), 'facility_type'] = 'after_school'
+        self.df.loc[self.df['facility_type'].str.contains('tavern|pub|brew|wine_tasting|bar_grill|hooka', case=False, na=None), 'facility_type'] = 'bar'
+        self.df.loc[self.df['facility_type'].str.contains('bakery', case=False, na=None), 'facility_type'] = 'bakery'
+        self.df.loc[self.df['facility_type'].str.contains('mobil|truck|mfd', case=False, na=None), 'facility_type'] = 'mobile_food'
+        self.df.loc[self.df['facility_type'].str.contains('kitchen', case=False, na=None), 'facility_type'] = 'kitchen'
+        self.df.loc[self.df['facility_type'].str.contains('restaurant|rstaurant|diner', case=False, na=None), 'facility_type'] = 'restaurant'
+        self.df.loc[self.df['facility_type'].str.contains('retail', case=False, na=None), 'facility_type'] = 'retail'
+        self.df.loc[self.df['facility_type'].str.contains('roof', case=False, na=None), 'facility_type'] = 'rooftop'
+        self.df.loc[self.df['facility_type'].str.contains('grocery', case=False, na=None), 'facility_type'] = 'grocery_store'
+        self.df.loc[self.df['facility_type'].str.contains('liquor', case=False, na=None), 'facility_type'] = 'liquor'
+        self.df.loc[self.df['facility_type'].str.contains('popup', case=False, na=None), 'facility_type'] = 'popup_establishment'
+        self.df.loc[self.df['facility_type'].str.contains('school|college|shcool', case=False, na=None), 'facility_type'] = 'school'
+        self.df.loc[self.df['facility_type'].str.contains('daycare', case=False, na=None), 'facility_type'] = 'daycare'
+        self.df.loc[self.df['facility_type'].str.contains('cafeteria|coffee|cafe', case=False, na=None), 'facility_type'] = 'coffee'
+        self.df.loc[self.df['facility_type'].str.contains('drug_store|pharmacy', case=False, na=None), 'facility_type'] = 'drug_store'
+        self.df.loc[self.df['facility_type'].str.contains('gym|fitness|weight_loss|exercise', case=False, na=None), 'facility_type'] = 'gym'
+        self.df.loc[self.df['facility_type'].str.contains('commissary|machine|commiasary', case=False, na=None), 'facility_type'] = 'vending_machine'
+        self.df.loc[self.df['facility_type'].str.contains('ice_cream|paleteria|gelato', case=False, na=None), 'facility_type'] = 'ice_cream'
+        self.df.loc[self.df['facility_type'].str.contains('banquet', case=False, na=None), 'facility_type'] = 'banquet'
+        self.df.loc[self.df['facility_type'].str.contains('lounge', case=False, na=None), 'facility_type'] = 'lounge'
+        self.df.loc[self.df['facility_type'].str.contains('church|religious', case=False, na=None), 'facility_type'] = 'church'
+        self.df.loc[self.df['facility_type'].str.contains('kiosk', case=False, na=None), 'facility_type'] = 'kiosk'
+        self.df.loc[self.df['facility_type'].str.contains('health|rehab', case=False, na=None), 'facility_type'] = 'health'
+        self.df.loc[self.df['facility_type'].str.contains('event', case=False, na=None), 'facility_type'] = 'events'
+        self.df.loc[self.df['facility_type'].str.contains('donut|hotdog|hot_dog|popcorn|juice|tea|dessert|deli|salad|snack|candy|shake|watermelon|smoothie|food|sushi', case=False, na=None), 'facility_type'] = 'other_food'
+        self.df.loc[self.df['facility_type'].str.contains('poultry|butcher|slaughter|meat', case=False, na=None), 'facility_type'] = 'butcher'
+        self.df.loc[self.df['facility_type'].str.contains('profit', case=False, na=None), 'facility_type'] = 'non_profit'
+        #self.df.loc[self.df['facility_type'].str.contains('na', case=False, na=None), 'facility_type'] = 'not_specified'
 
+    def _clean_inspection_type(self):
+        self.df.loc[self.df['inspection_type'].str.contains('license', case=False, na=None), 'inspection_type'] = 'license'
+        self.df.loc[self.df['inspection_type'].str.contains('task_force|taskforce', case=False, na=None), 'inspection_type'] = 'task_force'
+        self.df.loc[self.df['inspection_type'].str.contains('canvass|canvas', case=False, na=None), 'inspection_type'] = 'canvas'
+        self.df.loc[self.df['inspection_type'].str.contains('complaint', case=False, na=None), 'inspection_type'] = 'complaint'
+        self.df.loc[self.df['inspection_type'].str.contains('food|sick', case=False, na=None), 'inspection_type'] = 'suspected_food_poisoning'
+        
+    def _crea_num_violations(self):
+        self.df['num_violations'] = self.df['violations'].apply(lambda x: x.count(' | ') + 1 if x != 'na' else 0)        
+
+###
 
     def clean_data(self, save=False):
         print("Cleaning records..")
         self.original_rows, self.original_cols = self.df.shape
-        self.df.dropna()
+        #Codigo Agregado MH
+        self._subset_cols()
+        self._fill_nas()
+        self._change_data_types()
+        self._clean_results()
+        self._standarize_column_strings(['facility_type','risk','inspection_type'])
+        self._clean_facility_type()
+        self._clean_inspection_type()
+        self._crea_num_violations()
+        ###
         self.final_rows, self.final_cols = self.df.shape
         print("Records are clean and ready to be uploaded")
         if save:
@@ -43,6 +133,9 @@ class DataCleaner:
         print(f"Succesfully saved temp file as pickle in: {local_path}")
 
     def get_clean_df(self):
+        #Codigo agregado MH
+        self.clean_data()
+        #
         return self.df
 
     def get_cleaning_metadata(self):
