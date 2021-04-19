f7c9f61939918a70515790aa294d6a89baabf842
lechugaa <>
2021-04-19 10:29:37 -0500

    minor fix

--- sql/create_metadata_tables.sql
+++ sql/create_metadata_tables.sql
@@ -1,14 +1,14 @@
 CREATE TABLE test_ingestion_metadata (
-	ingestion_date DATE NOT NULL,
-	historic BOOLEAN NOT NULL,
-	num_obs INT NOT NULL,
-	data_size FLOAT NOT NULL
+    ingestion_date DATE NOT NULL,
+    historic BOOLEAN NOT NULL,
+    num_obs INT NOT NULL,
+    data_size FLOAT NOT NULL
 );
 
 CREATE TABLE test_upload_metadata (
-	ingestion_date DATE NOT NULL,
-	historic BOOLEAN NOT NULL,
-	file_name VARCHAR(250) NOT NULL
+    ingestion_date DATE NOT NULL,
+    historic BOOLEAN NOT NULL,
+    file_name VARCHAR(250) NOT NULL
 );
 
 CREATE TABLE test_clean_metadata (
@@ -16,8 +16,8 @@ CREATE TABLE test_clean_metadata (
     original_cols INT NOT NULL,
     final_rows INT NOT NULL,
     final_cols INT NOT NULL,
-	historic BOOLEAN NOT NULL,
-	ingestion_date DATE NOT NULL
+    historic BOOLEAN NOT NULL,
+    ingestion_date DATE NOT NULL
 );
 
 CREATE TABLE test_fe_metadata (
@@ -25,6 +25,6 @@ CREATE TABLE test_fe_metadata (
     original_cols INT NOT NULL,
     final_rows INT NOT NULL,
     final_cols INT NOT NULL,
-	historic BOOLEAN NOT NULL,
-	ingestion_date DATE NOT NULL
+    historic BOOLEAN NOT NULL,
+    ingestion_date DATE NOT NULL
 );
