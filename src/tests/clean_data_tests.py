a588542784351db36f818a1f123611d8b2d19e11
EnriqueOrtiz27 <enriqueortizcasillas@gmail.com>
2021-04-24 07:44:26 -0500

    clean tests

--- src/tests/clean_data_tests.py
+++ src/tests/clean_data_tests.py
@@ -14,14 +14,30 @@ class CleanTest(unittest.TestCase):
 
     def setUp(self):
         # getting local pickle
-        file_path = get_file_path(
-            historic=self.historic, query_date=self.query_date, prefix=DataCleaner.prefix)
-        self.df = load_from_pickle(file_path)
+        file_path = get_file_path(historic=self.historic, query_date=self.query_date, prefix=DataCleaner.prefix)
+        self.clean_df = load_from_pickle(file_path)
+
+        ingestion_path = get_file_path(historic=self.historic, query_date=self.query_date)
+        ingestion_data = load_from_pickle(ingestion_path)
+        self.original_df = pd.DataFrame.from_dict(ingestion_data)
         
-    def test_df_not_empty(self):
-        assert self.df.shape[0] != 0, "Df is empty"
+    def test_df_smaller_equal(self):
+        assert self.clean_df.shape[1] <= self.original_df.shape[1], "Added extra columns!"
+
+    def test_df_not_equal(self):
+        assert self.clean_df.shape[0] != 0, "Df is empty..."
+
+    def test_df_smaller_rows(self):
+        assert self.clean_df.shape[0] <= self.original_df[0], "Added extra rows!"
+
+    def test_df_columns(self):
+        expected_columns = ['inspection_id', 'dba_name', 'aka_name', 'license_', 'facility_type', 'risk', 'address',
+                            'city', 'state', 'zip', 'inspection_date', 'inspection_type', 'results', 'latitude',
+                            'longitude', 'location', 'violations']
+        assert set(self.clean_df.columns).issubset(expected_columns), "Cleaning: ¿Qué columnas has agregado?"
+
 
     def runTest(self):
-        print("Corriendo tests de almacenamiento...")
+        print("Corriendo tests de limpieza de datos...")
         self.test_df_are_equal()
-        print("Tests de almacenamiento terminados...")
+        print(">>>> Tests de limpieza terminados <<<<")
