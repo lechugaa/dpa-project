aa9bb145ba669e490c0cb73791f10c5df9981d62
EnriqueOrtiz27 <enriqueortizcasillas@gmail.com>
2021-04-30 17:51:24 -0500

    bias fairness class

--- src/pipeline/bias_fairness.py
+++ src/pipeline/bias_fairness.py
@@ -3,7 +3,7 @@ from aequitas.bias import Bias
 from aequitas.fairness import Fairness
 import datetime
 import os
-import pandas
+import pandas as pd
 import pickle
 
 from src.pipeline.limpieza_feature_eng import DataEngineer
@@ -24,6 +24,7 @@ class MrFairness:
         self.prefix = MrFairness.prefix
         self._load_data()
         self._load_model()
+        self.predictions = self.model.predict(self.features)  # predicciones
 
     def _load_data(self):
         """
@@ -51,3 +52,19 @@ class MrFairness:
         print(f"*** Selected model in previous task: {self.model} ***")
         # nota: aquí es cuando encuentro confuso el parámetro  training: los modelos no lo tienen
         # y por eso no lo pueden heredar de la clase
+
+    def _construct_aequitas_frame(self):
+        """
+        Dado que ya se hizo el one hot encoding, esto es un ligero dolor.
+        """
+        facility_types = [e for e in self.features.columns if e.startswith('facility')]
+        chosen_facilities = [0] * self.features.shape[0]
+        for i in range(len(self.features)):
+            for facility_type in facility_types:
+                if self.features[facility_type][i] == 1:
+                    chosen_facilities[i] = facility_type
+                    break
+
+        self.aequitas_df = pd.DataFrame({'score': self.predictions, 'label_value': self.labels,
+                                         'facility_type': chosen_facilities})
+        print(f"Successfully constructed aequitas dataframe with columns: {self.aequitas_df.columns}")
