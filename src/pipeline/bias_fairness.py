0b5cb9c996474f5a533e8d26c9c76fd854ae95c3
EnriqueOrtiz27 <enriqueortizcasillas@gmail.com>
2021-05-03 10:41:06 -0500

    bias fairness

--- src/pipeline/bias_fairness.py
+++ src/pipeline/bias_fairness.py
@@ -21,11 +21,14 @@ class MrFairness:
         self.query_date = query_date
         self.training = training
         self.prefix = MrFairness.prefix
+        self.alpha_bias = 0.05  # se usa en la sección de bias
+        self.protected_group = 'facility_type_restaurant'
         self._load_data()
         self._load_model()
         self.predictions = self.model.predict(self.features)  # predicciones
         self._construct_aequitas_frame()
         self._compute_group_metrics()
+        self._compute_bias_metrics()
 
     def _load_data(self):
         """
@@ -58,7 +61,8 @@ class MrFairness:
         """
         Dado que ya se hizo el one hot encoding, esto es un ligero dolor.
         """
-        facility_types = [e for e in self.features.columns if e.startswith('facility')]
+        facility_types = [
+            e for e in self.features.columns if e.startswith('facility')]
         chosen_facilities = [0] * self.features.shape[0]
         for i in range(len(self.features)):
             for facility_type in facility_types:
@@ -68,32 +72,53 @@ class MrFairness:
 
         self.aequitas_df = pd.DataFrame({'score': self.predictions, 'label_value': self.labels,
                                          'facility_type': chosen_facilities})
-        print(f"\n>>> Successfully constructed aequitas dataframe with columns: {self.aequitas_df.columns.values} <<<")
-
+        print(
+            f"\n>>> Successfully constructed aequitas dataframe with columns: {self.aequitas_df.columns.values} <<<")
 
     def _compute_group_metrics(self):
         """
-        Método para calcular y guardar las métricas iniciales de grupo, tanto 
-        absolutas como relativas.
+        Método para calcular y guardar las métricas iniciales de grupo, tanto
+        absolutas como relativas. Los resultados se guardan como dataframe.
         """
         group = Group()
-        self.all_metrics_df, self.attributes = group.get_crosstabs(self.aequitas_df)
-        self.absolute_metrics = group.list_absolute_metrics(self.all_metrics_df)
+        self.all_metrics_df, self.attributes = group.get_crosstabs(
+            self.aequitas_df)
+        self.absolute_metrics = group.list_absolute_metrics(
+            self.all_metrics_df)
         self._get_group_dataframes()
-    
+
     def _get_group_dataframes(self):
         """
-        Función para construir los dos dataframes que muestra liliana en su 
-        notebook: uno para conteos absolutos y otro para los relativos. 
+        Función para construir los dos dataframes que muestra liliana en su
+        notebook: uno para conteos absolutos y otro para los relativos.
         """
         # Primero: conteos absolutos
-        columns = [col for col in self.all_metrics_df if col not in self.absolute_metrics]
+        columns = [
+            col for col in self.all_metrics_df if col not in self.absolute_metrics]
         self.group_counts_df = self.all_metrics_df[columns]
 
-        #Luego: conteos como porcentaje
+        # Luego: conteos como porcentaje
         columns = ['attribute_name', 'attribute_value'] + self.absolute_metrics
         self.group_pct_df = self.all_metrics_df[columns].round(2)
-        print("\nSuccessfully constructed Group DataFrames.")
+        print("\nSuccessfully constructed Group dataframes: 'group_counts_df' and 'group_pct_df'")
+
+    def _compute_bias_metrics(self):
+        """
+        Método para calcular las métricas relevantes para la sección de sesgo, o Bias,
+        y guardarlas en un dataframe.
+        """
+        b = Bias()
+        # 46 columnas
+        self.extended_bias_df = b.get_disparity_predefined_groups(self.all_metrics_df,
+                                                                  original_df=self.aequitas_df, ref_groups_dict={
+                                                                      'facility_type': self.protected_group},
+                                                                  alpha=self.alpha_bias)
+
+        self.bias_metrics = b.list_disparities(self.extended_bias_df)
+        important_columns = ['attribute_name', 'attribute_value'] + self.bias_metrics
+        self.bias_df = self.extended_bias_df[important_columns].round(2)
+        print("\nSuccessfully constructed Bias dataframes: 'extended_bias_df' and 'bias_df'")
+
 
 
 """
@@ -102,5 +127,5 @@ from src.pipeline.bias_fairness import MrFairness
 from datetime import datetime
 date = datetime(2021, 4, 30)
 fair = MrFairness(historic=False, query_date=date, training=True)
-## 
+##
 """
