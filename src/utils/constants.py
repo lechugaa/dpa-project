6dec0c8c3007fc65cbd1e6cf90f54bf49653b357
Antonio Lechuga <antoniolechugarivera@gmail.com>
2021-05-22 11:24:50 -0500

    Update constants.py

--- src/utils/constants.py
+++ src/utils/constants.py
@@ -4,3 +4,4 @@ Documento de constantes para el proyecto
 
 bucket_name = "data-product-architecture-equipo-7"
 project_root = "~/dpa-project"
+models_prefix = "model-selection/initial/historic-inspections-"
